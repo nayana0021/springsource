a3caccafdd7402994c12332157aa168b800b791c
nayana0021 <qazzxx00@gmail.com>
2023-05-23 23:14:16 +0900

    rest board-rest Talend-댓글가져오기

---
+++ board_webmvc/src/main/java/com/spring/domain/ReplyDTO.java
@@ -0,0 +1,22 @@
+package com.spring.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter @Getter
+public class ReplyDTO {
+	private int rno;
+	private int bno;
+	private String reply;
+	private String replyer;
+	private Date replyDate;
+	
+}
